14a9d375c6633e9b54c1f196bd2f6b2778d0345d
Om Tiwari <work.om.tiwari@gmail.com>
2025-12-22 22:59:21 +0530

    Use env vars for SECRET_KEY and DEBUG

--- PDS/settings.py
+++ PDS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,11 +23,10 @@ MEDIA_URL = '/media/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vkpdmhr0q&8=zfwa9^zqi&bj-ry)))y7jobds2%26sypioijoz'
+SECRET_KEY = os.getenv("SECRET_KEY", "d19bc601c9dbd74442a4b864eb97f9ec1")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
 
 ALLOWED_HOSTS = ["*"]
 
