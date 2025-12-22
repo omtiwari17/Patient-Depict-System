b81bd5b7dde66ada0e30305c9f95e0aecd73ce85
Om Tiwari <work.om.tiwari@gmail.com>
2025-12-22 22:37:17 +0530

    Configure WhiteNoise and staticfiles

--- PDS/settings.py
+++ PDS/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-vkpdmhr0q&8=zfwa9^zqi&bj-ry)))y7jobds2%26sypioijoz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
